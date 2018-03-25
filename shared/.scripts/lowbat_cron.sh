c81c53533fd19ffe99d4bfe9e27d9712a833147a
Carl Egbert <egbertcarl@gmail.com>
2018-03-25 12:30:49 -0700

    remove laptop scripts from shared dir

--- shared/.scripts/lowbat_cron.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#
-# check if battery is low, and notify-send if it is. to use, assign to a cronjob.
-# you will need to preface with `export DISPLAY=:0 &&` in the cronjob because of notify-send.
-
-battlevel=$(acpi -b)
-if [[ "$battlevel" == *"Discharging"* && $(echo "$battlevel" | awk '{print ($(NF-2)-0)}') -lt "15" ]]; then
-  notify-send "LOW BATTERY" "$(echo "$battlevel" | awk '{ print $5 }') remaining"
-fi
