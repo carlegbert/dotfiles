3e2fe64d107f10ac02a23cca6b3d0e8a5218d800
Carl Egbert <egbertcarl@gmail.com>
2024-10-19 21:45:06 -0700

    lint

---
+++ .config/nvim/lua/plugins/lint.lua
@@ -0,0 +1,16 @@
+return {
+  'mfusseneger/nvim-lint',
+  event = { 'BufReadPre', 'BufNewFile' },
+  config = function()
+    local lint = require('lint')
+
+    lint.linters_by_ft = {
+      typescript = { 'eslint' },
+      typescriptreact = { 'eslint' },
+    }
+
+    vim.api.nvim_create_autocmd({ 'BufWritePost', 'BufReadPost', 'InsertLeave' }, {
+      callback = require('lint').try_lint
+    })
+  end
+}
